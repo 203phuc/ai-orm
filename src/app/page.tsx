63326492e41b3987f441cf60659e5dc3e11669bd
Tran Hoang Phuc <phuctranhoang203@gmail.com>
2026-05-22 20:59:38 +0700

    fix(UI-02): warp dashboard to fix client code run on server of fetchfeild component - 22/5/2026

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-import { supabase } from "@/database/dbconnect";
+import { Suspense } from "react";
 import Dashboard from "./component/Dashboard";
 
 export default async function TestPage() {
-  const { data, error } = await supabase.from("reviews").select("*");
-  console.log("ERROR:", error);
-
   return (
     <>
-      <Dashboard />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Dashboard />
+      </Suspense>
     </>
   );
 }
